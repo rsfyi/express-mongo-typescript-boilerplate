74dbe242e24b49ce4f8b1f2a0784c7347f3db538
Rahul Singh <rahul@nurtr.com>
2019-06-03 19:05:28 +0530

    added security packages

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express'
 
-import { userRoutes } from './user.route';
+import { userRoutes } from './user.route'
 
 const router = Router();
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.json({ message: "Hello world express" });
+});
 
 router.use('/user', userRoutes);
 
--- server/server.ts
+++ server/server.ts
@@ -1,38 +1,59 @@
-import { Request, Response, NextFunction } from 'express';
-import { Server } from 'http';
-import * as express from 'express';
-import * as cors from 'cors';
-import * as morgan from 'morgan';
-import * as bodyParser from 'body-parser';
+import * as bodyParser from 'body-parser'
+import * as compression from 'compression'
+import * as cors from 'cors'
+import * as csurf from 'csurf'
+import * as express from 'express'
+import * as helmet from 'helmet'
+import * as morgan from 'morgan'
+import rateLimit from 'express-rate-limit'
+import { NextFunction, Request, Response } from 'express'
 
-import { dbCon } from './models';
-
-import router from './routes';
+import router from './routes'
+import { dbCon } from './models'
 
 const port = 4100;
 
 const app = express();
-const httpServer = new Server(app);
 
-app.use(morgan('dev'));
+/**
+ *  security checks
+ */
+app.use(helmet());
 app.use(cors());
+// app.use(csurf());
+// app.use(
+// 	rateLimit({
+// 		windowMs: 15 * 60 * 1000, // 15 minutes
+// 		max: 100 // limit each IP to 100 requests per windowMs
+// 	})
+// );
+
+/**
+ * parse data to json
+ */
 app.use(bodyParser.json());
+app.use(compression());
+
+/**
+ * log data to console
+ */
+app.use(morgan('dev'));
 
 app.use('/', router);
 
 app.use('**', (req: Request, res: Response, next: NextFunction) => {
-    next({ message: 'No Routes found' });
+	next({ message: 'No Routes found' });
 });
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    res.json({ code: 422, message: err.message });
+	res.json({ code: 422, message: err.message });
 });
 
-httpServer.listen(port, async () => {
-    try {
-        await dbCon();
-        console.log(`Server is listening at http://localhost:${port}`);
-    } catch (e) {
-        console.log('Error ...', e.message);
-    }
+app.listen(port, async () => {
+	try {
+		// await dbCon();
+		console.log(`Server is listening at http://localhost:${port}`);
+	} catch (e) {
+		console.log('Error ...', e.message);
+	}
 });
